1ca2c915826a785fb822e7ba7c301e6d679704f6
Joshua Antikainen <64824757+ritzierwizard@users.noreply.github.com>
2025-05-19 16:24:35 -0600

    Update Typst.ps1

--- Powershell Scripts/Functions/Typst.ps1
+++ Powershell Scripts/Functions/Typst.ps1
@@ -10,7 +10,7 @@ function Add-TypstProject {
         }
         $newFolder = $Path + $Name
         if (!(Test-Path $newFolder -PathType Container)) {
-            Copy-Item -Recurse "$env:userprofile\OneDrive\Documents\Sublime Templates\sublime_typst" $Name 
+            Copy-Item -Recurse "$env:DOCUMENTS\Sublime Templates\sublime_typst" $Name 
             Set-Location $Name
         }
     }
